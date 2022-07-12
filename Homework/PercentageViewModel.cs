d999bc76cbba3458d7e83e2d0e43200b4c1d0378
woke5176 <65066480+woke5176@users.noreply.github.com>
2022-07-12 23:00:26 +0530

    Add files via upload

---
+++ Homework/PercentageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExamples
+{
+    internal class PercentageViewModel
+    {
+        public string Name { get; set; }
+
+        public double Percentage { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿using LinqExamples.Services;
+
+namespace LinqExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new TestService();
+
+            //service.GenerateMarksCard();
+
+            //var highestScorerInMaths = service.HighestScorerIn("Maths");
+            //Console.WriteLine(highestScorerInMaths);
+
+            //var topFiveScorersInMaths = service.HighestScorerIn("maths");
+            //Console.WriteLine(topFiveScorersInMaths);
+
+            var totalScores = service.GetTotalScores();
+
+            foreach (var item in totalScores)
+            {
+                Console.WriteLine($"{item.StudentId}, {item.StudentName}, {item.TotalMarks}");
+            }
+        }
+    }
+}
