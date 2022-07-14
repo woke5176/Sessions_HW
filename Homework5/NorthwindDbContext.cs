c8e7ad800dc08dc34ec44a67e7a9f170e76c203e
woke5176 <65066480+woke5176@users.noreply.github.com>
2022-07-14 23:58:45 +0530

    Add files via upload

---
+++ Homework5/NorthwindDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreScaffolding
+{
+    internal class NorthwindDbContext
+    {
+        private DbContextOptions options;
+
+        public NorthwindDbContext(DbContextOptions options)
+        {
+            this.options = options;
+        }
+
+    }
+}
---
+++ Homework5/Program.cs
@@ -0,0 +1,84 @@
+﻿using EFCoreScaffolding.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace EFCoreScaffolding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                     .SetBasePath(Directory.GetCurrentDirectory())
+                     .AddJsonFile("appsettings.json");
+
+            var config = configuration.Build();
+
+            var connectionString = config.GetConnectionString("NorthwindDbConnection");
+
+            var options = new DbContextOptionsBuilder<NorthwindContext>()
+                                //.UseLazyLoadingProxies()
+                                .UseSqlServer(connectionString)
+                                .Options;
+
+            var dbContext = new NorthwindContext(options);
+
+            var customer = dbContext.Customers.Include(c => c.Orders).FirstOrDefault();
+            var userID = 1;
+
+
+            var emp = dbContext.Employees.Include(a => a.Territories).Where(e => e.EmployeeId == userID);
+
+            foreach (var e in emp)
+            {
+                foreach (var x in e.Territories)
+                {
+                    Console.WriteLine(x.TerritoryDescription);
+                }
+            }
+
+        
+            var employee_max_sales = dbContext.Employees.Include(a => (a.Orders).GroupBy(o => o.EmployeeId).Count());
+
+            employee_max_sales = employee_max_sales.Include(a => a.OrderDetail);
+
+            // query 3 
+
+
+            var prod = dbContext.Invoices.Select(x => new
+            {
+                productname
+                = x.ProductName,
+                ship_con = x.ShipCountry
+
+            }).ToList();
+            foreach(var x in prod)
+            {
+                Console.WriteLine(x.productname + " " + x.ship_con);
+
+            }
+
+            // query 4 
+
+                      var det = dbContext.OrderDetails
+                          .Select(a => new
+                          {
+                              productName = a.ProductName,
+                              total_quan = a.Quantity,
+                              sales = (double)(a.UnitPrice) * (1 - (a.Discount)) * (a.Quantity)
+
+                          }).ToList();
+            foreach (var x in det)
+            {
+                Console.WriteLine(x.productName + " " + x.total_quan + " " + x.sales);
+
+           
+
+           
+
+
+        }
+    }
+}
